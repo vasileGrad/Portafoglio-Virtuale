bfbfb282fcf698af1aff2400f63e49d2936f563b
Grad Vasile <vasile.grad2@gmail.com>
2021-12-20 14:44:29 +0200

    Add routing functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { FavoritesComponent } from './components/favorites/favorites.component';
 import { FavoriteComponent } from './components/favorite/favorite.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule } from '@angular/material/card';
-import { HttpClientModule } from '@angular/common/http';
 import { GraphicComponent } from './components/graphic/graphic.component';
 import { WalletComponent } from './components/wallet/wallet.component';
 import { CoinDetailsComponent } from './components/coin-details/coin-details.component';
+import { CurrentCurrencyComponent } from './components/current-currency/current-currency.component';
+import { TransactionComponent } from './components/transaction/transaction.component';
 
+import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
 import { NgxEchartsModule } from 'ngx-echarts';
-import { CurrentCurrencyComponent } from './components/current-currency/current-currency.component';
-import { TransactionComponent } from './components/transaction/transaction.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'history', component: TransactionComponent }
+]
 
 @NgModule({
   declarations: [
@@ -44,6 +51,7 @@ import { TransactionComponent } from './components/transaction/transaction.compo
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
     MatCardModule,
     MatButtonModule,
     MatTableModule,
